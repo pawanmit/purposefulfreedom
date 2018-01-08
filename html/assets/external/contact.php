7e86c9ae7f28fa68c9bd4a860c5fdec889590056
M820787 <pawan.mittal@macys.com>
2018-01-08 13:41:49 -0800

    testing contact page

--- html/assets/external/contact.php
+++ html/assets/external/contact.php
@@ -1,8 +1,8 @@
 <?php
 mb_internal_encoding("UTF-8");
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$name = "Pawan"
+$email = "mitpawan@gmail.com"
+$message = "Hi this is a hard coded message";
 
 $to = 'pawan@purposefulfreedom.net';
 $subject = 'Message From Your Website Contact Form';
